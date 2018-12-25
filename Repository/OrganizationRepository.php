20e475aa9bf35788f2ff65e154c2e3e3d22886fa
Vidy Videni <vidy.videni@gmail.com>
2018-12-25 16:41:22 +0800

    let repository classes extend entity repository from VideniRestBundle

--- Repository/OrganizationRepository.php
+++ Repository/OrganizationRepository.php
@@ -3,7 +3,7 @@
 namespace Pintushi\Bundle\OrganizationBundle\Repository;
 
 use Pintushi\Bundle\OrganizationBundle\Entity\Organization;
-use Pintushi\Bundle\CoreBundle\Doctrine\ORM\ServiceEntityRepository;
+use Videni\Bundle\RestBundle\Doctrine\ORM\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\Query;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
