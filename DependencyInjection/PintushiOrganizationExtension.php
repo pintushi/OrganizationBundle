549cc0473f17565018ea528dfb02ad7152fac0d1
Vidy Videni <vidy.videni@gmail.com>
2019-01-28 10:28:33 +0800

    remove DoctrineUtils component

--- DependencyInjection/PintushiOrganizationExtension.php
+++ DependencyInjection/PintushiOrganizationExtension.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+
 final class PintushiOrganizationExtension extends Extension
 {
     /**
