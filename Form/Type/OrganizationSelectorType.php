23f12349d25cb9b1c0c703e1f3864cd6fb86178f
Vidy Videni <vidy.videni@gmail.com>
2019-04-09 10:29:50 +0800

    create default query builder for grid

--- Form/Type/OrganizationSelectorType.php
+++ Form/Type/OrganizationSelectorType.php
@@ -5,7 +5,6 @@ namespace Pintushi\Bundle\OrganizationBundle\Form\Type;
 use Symfony\Component\Form\Extension\Core\Type as FormTypes;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\AbstractType;
-use Pintushi\Bundle\AutoBundle\Entity\CarModel;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Pintushi\Bundle\OrganizationBundle\Form\Type\OrganizationSelectType;
 use Pintushi\Bundle\OrganizationBundle\Provider\RequestBasedOrganizationProvider;
